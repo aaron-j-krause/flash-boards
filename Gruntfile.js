b3fd1256219361a854cce88427ee5b425ad605ee
Aaron Krause <aaronjkrause@gmail.com>
2015-03-10 22:25:50 -0700

    basic structure, dependencies, back-end

---
+++ Gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jscs');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.initConfig({
+    jshint:{
+      options:{
+        jshintrc:true
+      },
+    files:['*.js', 'lib/**/*.js', 'models/**/*.js',
+      'test/**/*-test.js', 'routes/**/*.js', 'app/**/*.js']
+    },
+
+    jscs: {
+      all: {
+        options: {
+          config:'.jscsrc'
+        },
+        files: {
+          src: ['*.js', 'lib/**/*.js', 'test/**/*-test.js',
+            'models/**/*.js', 'routes/**/*.js', 'app/**/*.js']
+        }
+      }
+    },
+
+    clean:{
+      build:{
+        src:['build/']
+      }
+    },
+
+    copy:{
+      build:{
+        expand: true,
+        cwd: 'app/',
+        src: ['**/*.html', '**/*.css'],
+        dest: 'build/',
+        flatten: false,
+        filter: 'isFile'
+      }
+    },
+
+    browserify:{
+      dev:{
+        src: ['app/js/**/*.js'],
+        dest: 'build/bundle.js'
+      },
+      options:{
+        transform:['reactify', 'debowerify']
+      }
+    },
+
+    watch:{
+      files:['app/js/**/*.js', 'app/**/*.html', 'app/**/*.css'],
+      tasks:['clean', 'browserify', 'copy']
+    }
+  });
+
+  grunt.registerTask('default', ['jshint', 'jscs']);
+  grunt.registerTask('build', ['clean', 'browserify:dev', 'copy']);
+  grunt.registerTask('build:test', ['build:test']);
+
+};
